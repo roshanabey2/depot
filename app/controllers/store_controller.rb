4528d3707628d217c6924ad3838813179814d452
Roshan Abeysekera <roshanabey2@gmail.com>
2025-06-01 21:32:20 +0100

    Made a Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
+
+    @counter = session[:counter]
     @products = Product.order(:title)
   end
 end
